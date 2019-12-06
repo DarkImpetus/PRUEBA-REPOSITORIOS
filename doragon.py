93bd37039b6b26765994d0a039edef72680040a9
DarkImpetus <43411885+DarkImpetus@users.noreply.github.com>
2019-12-05 13:28:16 -1100

    PRUEBAS PARA ACOMPLETAR

---
+++ doragon.py
@@ -0,0 +1,23 @@
+a=12*(2+5.2)
+
+b = 2**3
+
+if a != 8:
+   a = 9
+else 
+   a = 10
+
+print
+for price in prices:
+	b = 10
+fro 
+while
+return
+void
+in
+def
+global
+try
+except
+
+#
---
+++ main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# desarollo de aanalizardor lexico
+
+import ply.lex as lex  # inportacion de librerias necesarias
+import re
+
+resultado_lexema = []
